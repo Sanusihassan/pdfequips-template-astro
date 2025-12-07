6e5dcd4835627ae96cb0e388925c1026b30e8580
Sanusi Hassan <sanusihassan122@gmail.com>
2025-12-07 21:58:51 +0100

    fixed no-fill issue

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,25 +4,27 @@ import { MdOutlineMoneyOffCsred } from "react-icons/md";
 import RobustIcon from "./icons/Robust";
 import { CiLock } from "react-icons/ci";
 
-export const Features = ({ features }: {
-    features: { title: string; description: string }[];
+export const Features = ({
+  features,
+}: {
+  features: { title: string; description: string }[];
 }) => {
-    const stateShowTool = useSelector(
-        (state: { tool: ToolState }) => state.tool.showTool
-    );
-    const icons = [MdOutlineMoneyOffCsred, RobustIcon, CiLock];
-    return (
-        <div className={`features${stateShowTool ? "" : " d-none"}`}>
-            {features.map(({ title, description }, i) => {
-                const Icon = icons[i];
-                return (
-                    <div className="feature" key={i}>
-                        <Icon className={`feature-icon${i === 1 ? " no-fill" : ""}`} />
-                        <div className="title">{title}</div>
-                        <p className="description">{description}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  const stateShowTool = useSelector(
+    (state: { tool: ToolState }) => state.tool.showTool
+  );
+  const icons = [MdOutlineMoneyOffCsred, RobustIcon, CiLock];
+  return (
+    <div className={`features${stateShowTool ? "" : " d-none"}`}>
+      {features.map(({ title, description }, i) => {
+        const Icon = icons[i];
+        return (
+          <div className="feature" key={i}>
+            <Icon className={`feature-icon${i === 2 ? " no-fill" : ""}`} />
+            <div className="title">{title}</div>
+            <p className="description">{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
